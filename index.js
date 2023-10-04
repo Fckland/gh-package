5c0fe1bd7a01663ddcc98fa1afe45bd6e8a68f06
Fckland <matt.fckland@gmail.com>
2023-10-04 12:39:18 -0300

    third-commit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const { Schema, model } = require('mongoose');
+
 const sayHello = (name) => {
   console.log('Hello ' + name + '!');
 };
 
+
+const ErrorSchema = new Schema(
+  {
+    backend: { type: String },
+    errorMessage: { type: String },
+    errorContext: { type: Object },
+    slackLoggedAt: { type: Date },
+    previousOccurredAt: { type: Date },
+  },
+  { timestamps: true }
+);
+
+const error = model('Error', ErrorSchema);
+
 module.exports = {
+  error,
   sayHello,
 };
